643df57062de0069e47674949db9b858cdac3f3d
Renan Beraldi <beraldi2019renan@gmail.com>
2020-09-14 16:18:17 -0300

    Program Ended

---
+++ Creating_Personalized_Exceptions3/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Creating_Personalized_Exceptions3.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Creating_Personalized_Exceptions3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Creating_Personalized_Exceptions3.Entities;
+using Creating_Personalized_Exceptions3.Entities.Exceptions;
+
+namespace Creating_Personalized_Exceptions3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room Number: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine(reservation);
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+                reservation.UpdateDates(checkIn, checkOut);
+
+                Console.WriteLine(reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+        }
+    }
+}
